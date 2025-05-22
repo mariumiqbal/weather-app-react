276a0eb24818905a0d7de7c1539509b18187df2e
Marium <maryam_cis10@hotmail.com>
2025-05-22 02:43:38 -0500

    Adding css for output layout

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import reactLogo from "./assets/react.svg";
 
 function App() {
   const getWeather = async (city: string, unit: string) => {
@@ -8,31 +9,51 @@ function App() {
   };
 
   return (
-    <>
-      <div className="weather-card">
-        <p>Enter city Below</p>
-        <input type="text" placeholder="Enter city name" className="input" />
-        <div className="radio-group">
-          <label className="circle-radio" htmlFor="fahrenheit">
-            <input
-              type="radio"
-              value="fahrenheit"
-              name="unit"
-              id="fahrenheit"
-              defaultChecked
-            />
-            <span className="dot"></span>F
-          </label>
-          <label className="circle-radio" htmlFor="celsius">
-            <input type="radio" value="celsius" name="unit" id="celsius" />
-            <span className="dot"></span>C
-          </label>
+    <div className="wrapper">
+      <div className="input-wrapper">
+        <div className="weather-card">
+          <p>Enter city Below</p>
+          <input type="text" placeholder="Enter city name" className="input" />
+          <div className="radio-group">
+            <label className="circle-radio" htmlFor="fahrenheit">
+              <input
+                type="radio"
+                value="fahrenheit"
+                name="unit"
+                id="fahrenheit"
+                defaultChecked
+              />
+              <span className="dot"></span>F
+            </label>
+            <label className="circle-radio" htmlFor="celsius">
+              <input type="radio" value="celsius" name="unit" id="celsius" />
+              <span className="dot"></span>C
+            </label>
+          </div>
+        </div>
+        <button onClick={() => alert("Button clicked!")}>Enter</button>
+      </div>
+      <div className="output-wrapper">
+        <div className="weather-forecast">
+          <img src={reactLogo} className="logo react" alt="weather icon" />
+          <h2> Temperature 52F</h2>
+          <h2>Chicago</h2>
+        </div>
+        <div className="favorite">
+          <input type="checkbox" id="checkbox" />
+          <label htmlFor="checkbox">Save as Favorite</label>
+        </div>
+        <div className="forecast">
+          <div className="forecast-day">
+            <p className="temp">52°F</p>
+          </div>
+          <div className="forecast-day">
+            <p className="temp">48°F</p>
+          </div>
+          <p className="forecast-label"> 3-5 day Forecast</p>
         </div>
       </div>
-      <button className="button" onClick={() => alert("Button clicked!")}>
-        Enter
-      </button>
-    </>
+    </div>
   );
 }
 
